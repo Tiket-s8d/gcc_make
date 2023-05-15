48e376b403d32a7825d8972d8e0e3ee169dac161
Tiket-s8d <timofei.nechaev@gmail.com>
2023-05-15 12:53:38 +0700

    В README.md была доблавлена строка про ключ -I. Так же написал tasks.json для компиляции кода через gcc

--- main.c
+++ main.c
@@ -1,9 +1,7 @@
-#include "stdio.h"
-#include "test.h"
-
-int add(int x, int y);
+#include <stdio.h>
+#include "Inc/test.h"
 
 int main(void){
-    printf("%d\n",add(5,2));
+    printf("%d\n",add(2,2));
     return 0;
 }
