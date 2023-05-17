e33cac47f51f15deed9ab2523d713483c3b68541
Tiket-s8d <timofei.nechaev@gmail.com>
2023-05-17 18:00:52 +0700

    Доделал

--- proj_cmake/test.bat
+++ proj_cmake/test.bat
@@ -1,13 +1,18 @@
-mkdir %~dp0\Debug
-mkdir %~dp0\Release
+@echo off
 
-cmake --help
-set /p input=Enter your name: 
+if  EXIST "%~dp0\CmakeLists.txt" (
+    mkdir %~dp0\Debug
+    mkdir %~dp0\Release
 
-cmake -G "%input%" -S %~dp0 -B %~dp0Debug
-cmake -DCMAKE_BUILD_TYPE=Debug -S %~dp0 -B %~dp0Debug
-cmake --build %~dp0\Debug
+    cmake --help
+    set /p input=Enter your name: 
 
-cmake -G "%input%" -S %~dp0 -B %~dp0Release
-cmake -DCMAKE_BUILD_TYPE=Release -S %~dp0 -B %~dp0Release
-cmake --build %~dp0\Release
+    cmake -G "%input%" -S %~dp0 -B %~dp0Debug
+    cmake -DCMAKE_BUILD_TYPE=Debug -S %~dp0 -B %~dp0Debug
+    cmake --build %~dp0\Debug
+
+    cmake -G "%input%" -S %~dp0 -B %~dp0Release
+    cmake -DCMAKE_BUILD_TYPE=Release -S %~dp0 -B %~dp0Release
+    cmake --build %~dp0\Release
+) else ( echo no)
+
