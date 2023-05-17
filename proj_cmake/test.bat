d0d16d78a5e24c72f08d492c78500044a94d4314
Tiket-s8d <timofei.nechaev@gmail.com>
2023-05-17 17:48:18 +0700

    добавлен файл для автоматической генерации и настроек директорий Debug Release

---
+++ proj_cmake/test.bat
@@ -0,0 +1,13 @@
+mkdir %~dp0\Debug
+mkdir %~dp0\Release
+
+cmake --help
+set /p input=Enter your name: 
+
+cmake -G "%input%" -S %~dp0 -B %~dp0Debug
+cmake -DCMAKE_BUILD_TYPE=Debug -S %~dp0 -B %~dp0Debug
+cmake --build %~dp0\Debug
+
+cmake -G "%input%" -S %~dp0 -B %~dp0Release
+cmake -DCMAKE_BUILD_TYPE=Release -S %~dp0 -B %~dp0Release
+cmake --build %~dp0\Release
