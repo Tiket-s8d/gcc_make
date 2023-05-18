a4ad60f32ed29adb8196ed3e500b465f0de0222c
Tiket-s8d <62832304+Tiket-s8d@users.noreply.github.com>
2023-05-18 16:02:28 +0700

    docker
    Проект для docker

---
+++ Sources/Preprocessing/Debug/CMakeFiles/MyApp.dir/Src/main.c.obj.d
@@ -0,0 +1,12 @@
+CMakeFiles/MyApp.dir/Src/main.c.obj: \
+ C:\Users\Tik\Documents\gcc_make\Sources\Preprocessing\Src\main.c \
+ C:/Users/Tik/Documents/gcc_make/Sources/Preprocessing/Inc/main.h \
+ C:/Users/Tik/Documents/gcc_make/Sources/Preprocessing/Inc/binTree.h \
+ c:\mingw\include\stdio.h c:\mingw\include\_mingw.h \
+ c:\mingw\include\msvcrtver.h c:\mingw\include\w32api.h \
+ c:\mingw\include\sdkddkver.h \
+ c:\mingw\lib\gcc\mingw32\6.3.0\include\stddef.h \
+ c:\mingw\include\sys/types.h \
+ c:\mingw\lib\gcc\mingw32\6.3.0\include\stdarg.h \
+ c:\mingw\include\stdlib.h c:\mingw\include\time.h \
+ c:\mingw\include\sys\types.h
---
+++ Sources/Preprocessing/Release/CMakeFiles/MyApp.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyApp.dir/Src/binTree.c.obj"
+  "CMakeFiles/MyApp.dir/Src/binTree.c.obj.d"
+  "CMakeFiles/MyApp.dir/Src/main.c.obj"
+  "CMakeFiles/MyApp.dir/Src/main.c.obj.d"
+  "MyApp.exe"
+  "MyApp.exe.manifest"
+  "MyApp.pdb"
+  "libMyApp.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/MyApp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
